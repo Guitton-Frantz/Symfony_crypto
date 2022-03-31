7c194ac6b56eb32fb1c97005c6999107b26625cf
Marius GUITTON-FRANTZ <marius.guitton-frantz@etu.univ-nantes.fr>
2022-03-31 12:38:07 +0200

    Fixtures ajout

--- ProjetCrypto/src/DataFixtures/UserFixtures.php
+++ ProjetCrypto/src/DataFixtures/UserFixtures.php
@@ -10,6 +10,9 @@ use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 class UserFixtures extends Fixture
 {
     public const TEST_USER_REFERENCE = 'test-user';
+    public const MARIUS_USER_REFERENCE = 'marius-user';
+    public const EVAN_USER_REFERENCE = 'evan-user';
+
     private $passwordEncoder;
 
     public function __construct(UserPasswordEncoderInterface $passwordEncoder)
@@ -25,9 +28,30 @@ class UserFixtures extends Fixture
             ->setPassword($this->passwordEncoder->encodePassword(
             $userTest, 'test'
         ));
+
+        $userMarius = new User();
+        $userMarius->setEmail("marius@gmail.com")
+            ->setRoles(["ROLE_USER"])
+            ->setPassword($this->passwordEncoder->encodePassword(
+                $userMarius, 'marius'
+            ));
+
+        $userEvan = new User();
+        $userEvan->setEmail("evan@gmail.com")
+            ->setRoles(["ROLE_USER"])
+            ->setPassword($this->passwordEncoder->encodePassword(
+                $userEvan, 'evan'
+            ));
+
         $manager->persist($userTest);
+        $manager->persist($userMarius);
+        $manager->persist($userEvan);
+
         $manager->flush();
         // other fixtures can get this object using the UserFixtures::TEST_USER_REFERENCE constant
         $this->addReference(self::TEST_USER_REFERENCE, $userTest);
+        $this->addReference(self::MARIUS_USER_REFERENCE, $userMarius);
+        $this->addReference(self::EVAN_USER_REFERENCE, $userEvan);
+
     }
 }
